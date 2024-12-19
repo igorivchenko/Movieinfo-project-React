60ef17102b7e57ee396d15ebacf5e024ed0e178b
Denis <denistaran482@gmail.com>
2024-12-19 14:55:07 +0200

    Add project to main GitHub account

--- src/components/ErrorNotice/ErrorNotice.jsx
+++ src/components/ErrorNotice/ErrorNotice.jsx
@@ -1,4 +1,4 @@
 const ErrorNotice = () => {
-  return <p style={{ color: 'red' }}>Sorry, Something went wrong... Try again later.</p>;
+  return <p style={{ color: 'red' }}>Sorry, Something went wrong... Try again later</p>;
 };
 export default ErrorNotice;
