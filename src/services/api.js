7ec46f58639bddf88c73555ad4cabb6d28acf5ac
igorivchenko <denistaran482@gmail.com>
2025-01-06 16:32:18 +0200

    update project

--- src/services/api.js
+++ src/services/api.js
@@ -12,6 +12,7 @@ axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
 const fetchTrendingMovies = async () => {
   const { data } = await axios.get('/trending/movie/day?language=en-US', options);
+  console.log(data);
   return data;
 };
 
